james424smith/react-native-web

commit 217ad97bfd3d4caf2ad04dd59ad9f04d7ec630e7

[change] Update Flow and types

src/apis/AppRegistry/AppContainer.js
@@ -19,6 +19,7 @@ type Context = {
 };
 
 type Props = {
+  // $FlowFixMe
   children?: React.Children,
   rootTag: any
 };
@@ -27,10 +28,7 @@ type State = {
   mainKey: number
 };
 
-export default class AppContainer extends Component {
-  props: Props;
-  state: State = { mainKey: 1 };
-
+export default class AppContainer extends Component<Props, State> {
   static childContextTypes = {
     rootTag: any
   };

src/apis/AppRegistry/index.js
@@ -13,16 +13,18 @@
 import invariant from 'fbjs/lib/invariant';
 import { unmountComponentAtNode } from 'react-dom';
 import renderApplication, { getApplication } from './renderApplication';
+import type { ComponentType } from 'react';
 
 const emptyObject = {};
 const runnables = {};
 
-export type ComponentProvider = () => ReactClass<any>;
+export type ComponentProvider = () => ComponentType<any>;
 
 export type AppConfig = {
   appKey: string,
   component?: ComponentProvider,
-  run?: Function
+  run?: Function,
+  section?: boolean
 };
 
 /**
@@ -90,7 +92,7 @@ export default class AppRegistry {
     runnables[appKey].run(appParameters);
   }
 
-  static unmountApplicationComponentAtRootTag(rootTag) {
+  static unmountApplicationComponentAtRootTag(rootTag: Object) {
     unmountComponentAtNode(rootTag);
   }
 }

src/apis/AppRegistry/renderApplication.js
@@ -13,11 +13,11 @@ import invariant from 'fbjs/lib/invariant';
 import { render } from 'react-dom';
 import AppContainer from './AppContainer';
 import StyleSheet from '../../apis/StyleSheet';
-import React from 'react';
+import React, { type ComponentType } from 'react';
 
-export default function renderApplication(
-  RootComponent: ReactClass<Object>,
-  initialProps: Object,
+export default function renderApplication<Props: Object>(
+  RootComponent: ComponentType<Props>,
+  initialProps: Props,
   rootTag: any
 ) {
   invariant(rootTag, 'Expect to have a valid rootTag, instead got ', rootTag);
@@ -30,7 +30,10 @@ export default function renderApplication(
   );
 }
 
-export function getApplication(RootComponent: ReactClass<Object>, initialProps: Object): Object {
+export function getApplication(
+  RootComponent: ComponentType<Object>,
+  initialProps: Object
+): Object {
   const element = (
     <AppContainer rootTag={{}}>
       <RootComponent {...initialProps} />

src/apis/AsyncStorage/index.js
@@ -20,7 +20,7 @@ const mergeLocalStorageItem = (key, value) => {
   window.localStorage.setItem(key, nextValue);
 };
 
-const createPromise = (getValue, callback) => {
+const createPromise = (getValue, callback): Promise<*> => {
   return new Promise((resolve, reject) => {
     try {
       const value = getValue();
@@ -37,7 +37,7 @@ const createPromise = (getValue, callback) => {
   });
 };
 
-const createPromiseAll = (promises, callback, processResult) => {
+const createPromiseAll = (promises, callback, processResult): Promise<*> => {
   return Promise.all(promises).then(
     result => {
       const value = processResult ? processResult(result) : null;
@@ -55,7 +55,7 @@ export default class AsyncStorage {
   /**
    * Erases *all* AsyncStorage for the domain.
    */
-  static clear(callback) {
+  static clear(callback?: Function): Promise<*> {
     return createPromise(() => {
       window.localStorage.clear();
     }, callback);
@@ -69,7 +69,7 @@ export default class AsyncStorage {
   /**
    * Gets *all* keys known to the app, for all callers, libraries, etc.
    */
-  static getAllKeys(callback) {
+  static getAllKeys(callback?: Function): Promise<*> {
     return createPromise(() => {
       const numberOfKeys = window.localStorage.length;
       const keys = [];
@@ -84,7 +84,7 @@ export default class AsyncStorage {
   /**
    * Fetches `key` value.
    */
-  static getItem(key: string, callback) {
+  static getItem(key: string, callback?: Function): Promise<*> {
     return createPromise(() => {
       return window.localStorage.getItem(key);
     }, callback);
@@ -96,7 +96,7 @@ export default class AsyncStorage {
    *
    *   multiGet(['k1', 'k2']) -> [['k1', 'val1'], ['k2', 'val2']]
    */
-  static multiGet(keys: Array<string>, callback) {
+  static multiGet(keys: Array<string>, callback?: Function): Promise<*> {
     const promises = keys.map(key => AsyncStorage.getItem(key));
     const processResult = result => result.map((value, i) => [keys[i], value]);
     return createPromiseAll(promises, callback, processResult);
@@ -105,7 +105,7 @@ export default class AsyncStorage {
   /**
    * Sets `value` for `key`.
    */
-  static setItem(key: string, value: string, callback) {
+  static setItem(key: string, value: string, callback?: Function): Promise<*> {
     return createPromise(() => {
       window.localStorage.setItem(key, value);
     }, callback);
@@ -115,7 +115,7 @@ export default class AsyncStorage {
    * Takes an array of key-value array pairs.
    *   multiSet([['k1', 'val1'], ['k2', 'val2']])
    */
-  static multiSet(keyValuePairs: Array<Array<string>>, callback) {
+  static multiSet(keyValuePairs: Array<Array<string>>, callback?: Function): Promise<*> {
     const promises = keyValuePairs.map(item => AsyncStorage.setItem(item[0], item[1]));
     return createPromiseAll(promises, callback);
   }
@@ -123,7 +123,7 @@ export default class AsyncStorage {
   /**
    * Merges existing value with input value, assuming they are stringified JSON.
    */
-  static mergeItem(key: string, value: string, callback) {
+  static mergeItem(key: string, value: string, callback?: Function): Promise<*> {
     return createPromise(() => {
       mergeLocalStorageItem(key, value);
     }, callback);
@@ -135,7 +135,7 @@ export default class AsyncStorage {
    *
    *   multiMerge([['k1', 'val1'], ['k2', 'val2']])
    */
-  static multiMerge(keyValuePairs: Array<Array<string>>, callback) {
+  static multiMerge(keyValuePairs: Array<Array<string>>, callback?: Function): Promise<*> {
     const promises = keyValuePairs.map(item => AsyncStorage.mergeItem(item[0], item[1]));
     return createPromiseAll(promises, callback);
   }
@@ -143,7 +143,7 @@ export default class AsyncStorage {
   /**
    * Removes a `key`
    */
-  static removeItem(key: string, callback) {
+  static removeItem(key: string, callback?: Function): Promise<*> {
     return createPromise(() => {
       return window.localStorage.removeItem(key);
     }, callback);
@@ -152,7 +152,7 @@ export default class AsyncStorage {
   /**
    * Delete all the keys in the `keys` array.
    */
-  static multiRemove(keys: Array<string>, callback) {
+  static multiRemove(keys: Array<string>, callback?: Function): Promise<*> {
     const promises = keys.map(key => AsyncStorage.removeItem(key));
     return createPromiseAll(promises, callback);
   }

src/apis/Clipboard/index.js
@@ -17,11 +17,11 @@ export default class Clipboard {
     );
   }
 
-  static getString() {
+  static getString(): Promise<string> {
     return Promise.resolve('');
   }
 
-  static setString(text) {
+  static setString(text: string) {
     let success = false;
     const body = document.body;
 

src/apis/Dimensions/index.js
@@ -66,12 +66,12 @@ export default class Dimensions {
     }
   }
 
-  static addEventListener(type, handler): void {
+  static addEventListener(type: string, handler: Function): void {
     listeners[type] = listeners[type] || [];
     listeners[type].push(handler);
   }
 
-  static removeEventListener(type, handler): void {
+  static removeEventListener(type: string, handler: Function): void {
     if (Array.isArray(listeners[type])) {
       listeners[type] = listeners[type].filter(_handler => _handler !== handler);
     }

src/apis/Linking/index.js
@@ -17,13 +17,13 @@ const initialURL = canUseDOM ? window.location.href : '';
 const Linking = {
   addEventListener() {},
   removeEventListener() {},
-  canOpenURL() {
+  canOpenURL(): Promise<boolean> {
     return Promise.resolve(true);
   },
-  getInitialURL() {
+  getInitialURL(): Promise<string> {
     return Promise.resolve(initialURL);
   },
-  openURL(url: string) {
+  openURL(url: string): Promise<Object | void> {
     try {
       iframeOpen(url);
       return Promise.resolve();

src/apis/StyleSheet/StyleSheetValidation.js
@@ -24,7 +24,7 @@ import { number, oneOf, string } from 'prop-types';
 const ReactPropTypesSecret = 'SECRET_DO_NOT_PASS_THIS_OR_YOU_WILL_BE_FIRED';
 
 export default class StyleSheetValidation {
-  static validateStyleProp(prop, style, caller) {
+  static validateStyleProp(prop: string, style: Object, caller: string) {
     if (process.env.NODE_ENV !== 'production') {
       const isCustomProperty = prop.indexOf('--') === 0;
       if (isCustomProperty) return;
@@ -51,7 +51,7 @@ export default class StyleSheetValidation {
     }
   }
 
-  static validateStyle(name, styles) {
+  static validateStyle(name: string, styles: Object) {
     if (process.env.NODE_ENV !== 'production') {
       for (const prop in styles[name]) {
         StyleSheetValidation.validateStyleProp(prop, styles[name], 'StyleSheet ' + name);
@@ -59,7 +59,7 @@ export default class StyleSheetValidation {
     }
   }
 
-  static addValidStylePropTypes(stylePropTypes) {
+  static addValidStylePropTypes(stylePropTypes: Object) {
     for (const key in stylePropTypes) {
       allStylePropTypes[key] = stylePropTypes[key];
     }

commit 0879be9f034e98ae5de62476092dfdb50ea9c5cb

[fix] AppContainer initial state
Regression introduced in:
217ad97bfd3d4caf2ad04dd59ad9f04d7ec630e7

Close #738

src/apis/AppRegistry/AppContainer.js
@@ -29,6 +29,8 @@ type State = {
 };
 
 export default class AppContainer extends Component<Props, State> {
+  state = { mainKey: 1 };
+
   static childContextTypes = {
     rootTag: any
   };